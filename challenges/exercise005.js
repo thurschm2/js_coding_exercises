09e5c916e97a0d559cecd85fd66a169fb934f90e
thurschm2 <thurschm2@gmail.com>
2021-02-24 08:51:26 +0000

    more attempts on challenge005 FindNeedle

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,88 +1,87 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-    let position = nums.indexOf(n);
-    if (position == -1) { return null }
-    else
-        if (position == nums.length - 1) { return null; }
-        else { return nums[position + 1] }
+  let position = nums.indexOf(n);
+  if (position == -1) { return null }
+  else
+    if (position == nums.length - 1) { return null; }
+    else { return nums[position + 1] }
   return null
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-    let nbZeros = (str.match(/0/g) || []).length;
-    let nbOnes = (str.match(/1/g) || []).length;
-    //console.log("0 ", nbZeros, " 1 ", nbOnes);
-    let obj = { 1: nbOnes, 0: nbZeros };
-    //console.log("object : ", obj)
+  let nbZeros = (str.match(/0/g) || []).length;
+  let nbOnes = (str.match(/1/g) || []).length;
+  let obj = { 1: nbOnes, 0: nbZeros };
 
-    return obj;
+  return obj;
 
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-    let numString = n.toString();
-    var answer = "";
-    for (var i = numString.length - 1; i >= 0; i--) {
-        answer = answer + numString[i]
-    }
-    return Number(answer);
+  let numString = n.toString();
+  var answer = "";
+  for (var i = numString.length - 1; i >= 0; i--) {
+    answer = answer + numString[i]
+  }
+  return Number(answer);
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-    let total = 0;
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    for (var i = 0; i < arrs.length; i++) {
-        
-        total += arrs[i].reduce(reducer);
-        
+  let total = 0;
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  for (var i = 0; i < arrs.length; i++) {
 
-    };
+    total += arrs[i].reduce(reducer);
 
-return total;
+
+  }
+
+  return total;
 };
 
 const arrShift = arr => {
-    if (arr === undefined) throw new Error("arr is required");
-    if (arr.length >1) {
-        let temp = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = temp;
-    }
-    return arr;
-   
+  if (arr === undefined) throw new Error("arr is required");
+  if (arr.length > 1) {
+    let temp = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp;
+  }
+  return arr;
+
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
-    if (searchTerm === undefined) throw new Error("searchTerm is required");
-    console.log(haystack, searchTerm);
-    console.log(Object.values(haystack).indexOf(searchTerm));
-    if (Object.values(haystack).indexOf(searchTerm) > -1) {
-        return true;
-    }
-    else return false;
+  if (searchTerm === undefined) throw new Error("searchTerm is required");
+  const toSearch = searchTerm.toLowerCase()
+  console.log(haystack, searchTerm, toSearch)
+  if (Object.values(haystack).indexOf(toSearch) > -1) {
+    return true;
+  }
+  else return false;
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
-    var wordsString = str;
-    var wordArray = wordsString.split(' ');
-    var wordCounter = [];
-    
-    for (var i = 0; i < wordArray.length; i++) {
-        let frequency = (str.match(/wordArray[i]/g) || []).length;
-        console.log(wordArray[i], frequency);
+  const wordsString = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+  const wordArray = wordsString.split(' ');
+  const answer = {};
+
+  for (var x = 0; x < wordArray.length; x++) {
+    if (answer[wordArray[x]] === undefined) {
+      answer[wordArray[x]] = 1;
+    } else {
+      answer[wordArray[x]]++;
     }
-    // need to create an object with result
-    /*var armixed = wordArray.map(function (x, y) {
-        return [x, wordCounter[y]]
-    });
-*/    return armixed;
+  }
+  //  console.log(answer);
+  return answer;
 };
+
 
 module.exports = {
   findNextNumber,
